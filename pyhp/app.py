070cc7d56ed3e4f1486398d75b9c9ac41c189881
YuviPanda <yuvipanda@gmail.com>
2020-12-15 17:30:02 +0530

    Explicitly list file extensions for static file serving
    Apache + PHP will happily serve our .git files, your credential
    files, your code files, etc.

--- pyhp/app.py
+++ pyhp/app.py
@@ -4,8 +4,23 @@ from jinja2 import Environment, FileSystemLoader, TemplateNotFound
 import os
 from .extension import PythonExtension
 
+# FIXME: Make this configurable
+BASE_DIR = os.getcwd()
+
+# File extensions we serve unchanged by default
+# This prevents the common PHP mistake of exposing your credentials file
+# as a common static file.
+STATIC_EXTENSIONS = [
+    # Base web stuff
+    '.css', '.js', '.html',
+    # Image formats
+    '.png', '.jpeg', '.svg', '.ico',
+    # webfonts
+    '.ttf', '.woff', '.woff2'
+]
+
 env = Environment(
-    loader=FileSystemLoader('.'),
+    loader=FileSystemLoader(BASE_DIR),
     extensions=[PythonExtension]
 )
 
@@ -13,20 +28,22 @@ app = Flask(__name__)
 logger = app.logger
 logger.setLevel(logging.INFO)
 
-BASE_DIR = os.getcwd()
 
 @app.route('/<path:path>')
 def render(path):
     _, ext = os.path.splitext(path)
     if ext != '.pyhp':
-        # FIXME: send_from_directory doesn't send same path as os.path.join(BASE_DIR, path)
-        # send_from_directory is more secure & protects against path traversal attacks
-        logger.info("Serving {path} from {BASE_DIR} as a static file")
-        return send_from_directory(BASE_DIR, path)
+        if ext in STATIC_EXTENSIONS:
+            # FIXME: send_from_directory doesn't send same path as os.path.join(BASE_DIR, path)
+            # send_from_directory is more secure & protects against path traversal attacks
+            logger.info("Serving {path} from {BASE_DIR} as a static file")
+            return send_from_directory(BASE_DIR, path)
+        else:
+            return make_response(('Access denied', 403))
     try:
         template = env.get_template(path)
     except TemplateNotFound:
-        return make_response(("No such file {path} found", 404))
+        return make_response((f"No such file {path} found", 404))
     return template.render()
 
 if __name__ == '__main__':
